e811a55c0002d9b423f1de4c28a144843cf91b31
Mariusz Różycki <mar.rozycki@gmail.com>
2021-08-20 21:39:35 +0200

    Add basic command line support

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    env, fs,
+    io::{self, Read, Result},
+};
+
+fn process(code: &str) {
+    println!("{}", code);
+}
+
+fn main() -> Result<()> {
+    let args: Vec<_> = env::args().collect();
+
+    if args.len() == 1 {
+        let mut code = String::new();
+        io::stdin().read_to_string(&mut code)?;
+        process(&code);
+    } else if args.len() == 2 {
+        let code = fs::read_to_string(&args[1])?;
+        process(&code);
+    } else {
+        eprintln!("Usage: {} [<filename>]", args[0]);
+    }
+
+    Ok(())
 }
